1430cc9c3c1a9bb7b5662188651462d6153028a5
Hehpheh <102379707+Hehpheh@users.noreply.github.com>
2024-04-15 12:38:13 +0300

    Add Tab Layout for navigation between login and registration fragments

--- app/src/main/java/com/example/myapplicationjava/MainActivity.java
+++ app/src/main/java/com/example/myapplicationjava/MainActivity.java
@@ -1,21 +1,34 @@
 package com.example.myapplicationjava;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
-import android.content.Intent;
+import com.google.android.material.tabs.TabLayout;
+import android.annotation.SuppressLint;
 import android.os.Bundle;
-import android.view.View;
+
 
 public class MainActivity extends AppCompatActivity {
-
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+ 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tabLayout=findViewById(R.id.tablayout);
+        viewPager=findViewById(R.id.viewpager);
+
+        tabLayout.setupWithViewPager(viewPager);
+
+        VPAdapter vpAdapter=new VPAdapter(getSupportFragmentManager(), FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+        vpAdapter.addFragment(new fragment1(), "Вход");
+        vpAdapter.addFragment(new fragment2(), "Регистрация");
+        viewPager.setAdapter(vpAdapter);
     }
 
-    public void startNewActivity(View v){
-        Intent intent= new Intent(this, RegistrationPage.class);
-        startActivity(intent);
-    }
+
+
 }
