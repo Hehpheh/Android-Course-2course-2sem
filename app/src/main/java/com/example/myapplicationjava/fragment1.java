a5c99de81f7fc95e9172ec759fd91dcdb5c18a2a
Hehpheh <102379707+Hehpheh@users.noreply.github.com>
2024-05-02 12:26:14 +0300

    Update fragment1.java

--- app/src/main/java/com/example/myapplicationjava/fragment1.java
+++ app/src/main/java/com/example/myapplicationjava/fragment1.java
@@ -15,7 +15,6 @@ public class fragment1 extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_1, container, false);
     }
 
